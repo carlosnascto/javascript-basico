24cf7b0d3e0817ef476aefd79e0af95c55a6df5e
Gleyson Sampaio <glysns@hotmail.com>
2022-11-29 15:00:47 -0300

    estudando sobre operadores em javascript

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript-basico
 Meu respositorio de estudos sobre os conceitos, fundamentos e recursos do Java Script baseado no ECMA6
+
+1. Ola mundo
+1. Teste
---
+++ operadores.js
@@ -0,0 +1,13 @@
+function contarDeUmADez(){
+    for(x=1; x<=10; x++) {
+      console.log(x);
+    }
+  }
+  
+  function somar(um, dois){
+    return um + dois;
+  }
+  
+  contarDeUmADez()
+  
+  somar(2,6)
