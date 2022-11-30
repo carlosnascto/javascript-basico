a3693363bf92bdb1122552237242181080b29f9a
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-11-30 15:39:20 -0300

    estudando tipos primitivos

---
+++ tipos primitivos.js
@@ -0,0 +1,5 @@
+let nome = 'Carlos'; // string literal
+let idade = 16; // number literal
+let solteiro = true;  // boolean
+let sobrenome;  // undefined
+let corOpcional = null; // redefinição de valor
