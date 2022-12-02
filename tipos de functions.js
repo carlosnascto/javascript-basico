03ba011172b65c2fc447bcd99c20ecdac23da33a
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-02 15:41:22 -0300

    estudando tipos de funções

---
+++ tipos de functions.js
@@ -0,0 +1,14 @@
+function dizerNumero (){
+  console.log(5);
+}
+
+dizerNumero();
+
+
+function multiplicarPorCinco (valor){
+  return valor * 5;
+}
+
+let resultado = multiplicarPorCinco(3);
+
+console.log(resultado);
