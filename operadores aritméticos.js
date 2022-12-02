5e083ac1f42c407887d2d0d3ce278fe9271cc3d3
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-02 16:16:25 -0300

    estudando sobre operadores aritméticos

---
+++ operadores aritméticos.js
@@ -0,0 +1,19 @@
+// OPERADORES ARITMÉTICOS
+// +, -, *, /, **
+
+let numero = 50 
+
+console.log(numero + numero);
+console.log(numero - numero);
+console.log(numero * numero);
+console.log(numero / numero);
+console.log(3 ** 3);
+
+// INCREMENTAÇÃO E DECREMENTAÇÃO
+// ++. --
+
+console.log(--numero);
+console.log(++numero);
+console.log(++numero);
+
+console.log(numero);
