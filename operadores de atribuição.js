157103cd08a7d4846738df70e0d4c1f761a99c6a
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-03 08:23:18 -0300

    estudando sobre operadores de atribuição

---
+++ operadores de atribuição.js
@@ -0,0 +1,8 @@
+// OPERADORES DE ATRIBUIÇÃO
+
+let valorProduto = 35;
+
+// valorProduto = valorProduto + valorProduto
+valorProduto += valorProduto
+// valorProduto = valorProduto - valorProduto
+valorProduto -= valorProduto
