54e41f5197460d38766dd3741bc218d1c3a60198
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-05 14:04:45 -0300

    estudando switch e case

---
+++ switch e case.js
@@ -0,0 +1,16 @@
+let permissão = 'professor';
+
+switch (permissão ){
+  case 'aluno':
+  console.log('usuário ' + permissão)
+  break;
+    
+  case 'professor':
+  console.log('usuário ' + permissão)
+  break;
+    
+  case 'diretor':
+  console.log('usuário ' + permissão)
+  break;
+  
+}
