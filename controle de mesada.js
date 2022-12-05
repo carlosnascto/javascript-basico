5848fa7decfe62f996c7e883f2e40c2c60ebcbac
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-05 15:07:25 -0300

    projeto de controle de mesada

---
+++ controle de mesada.js
@@ -0,0 +1,18 @@
+let mesada = 50;
+
+
+function valorAleatorio(n1, n2){
+  return Math.floor(Math.random() * (n2 - n1 + 1)) + n1
+}
+
+while(mesada > 0){
+  let valorDoce = valorAleatorio(2, 8);
+  if(valorDoce > mesada)
+   valorDoce = mesada;
+  
+  console.log("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+  mesada = mesada - valorDoce;
+}
+
+console.log("Mesada: " + mesada);
+console.log("João gastou toda a sua mesada em doces");
