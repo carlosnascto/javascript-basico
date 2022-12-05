6eb0deed3d1e73f566631f52d2436d2ceaa75a91
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-05 13:49:46 -0300

    estudando sobre os laços for e while

---
+++ for e while.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+  if (i === 5)  
+    break;
+  
+  console.log("O número é " + i)
+}
+
+
+let i = 0
+while(i < 10) {
+  if(i === 5) break;
+  
+  console.log("O número é " + i)
+  i++
+}
