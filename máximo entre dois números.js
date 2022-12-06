65dab310de2c366a8a04b40fee1c06871a958d24
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-06 09:13:03 -0300

    mini projeto: máximo entre dois números

---
+++ máximo entre dois números.js
@@ -0,0 +1,7 @@
+function max(n1, n2){ // função
+  return n1 > n2 ? n1 : n2; // operadores ternários
+}
+
+let maiorNumero = max(3,7); // variável recebe função
+console.log(maiorNumero) // imprimindo variável
+
