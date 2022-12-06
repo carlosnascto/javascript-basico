d702210eb115e6f17b91c909a62e4073e3df2678
Carlos Henrique <carlos.nascto1604@gmail.com>
2022-12-06 08:58:28 -0300

    estudando sobre for in e for of

---
+++ for in e for of.js
@@ -0,0 +1,13 @@
+// FOR IN AND FOR OF
+const frutas = ['banana', 'maçã', 'abacate', 'maracujá'];
+
+for (let fruta in frutas) {
+  console.log (fruta);
+}
+
+
+for (let fruta of frutas) {
+  console.log (fruta);
+}
+
+
