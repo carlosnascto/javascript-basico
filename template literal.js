85eb12ede2d485f5e7c093b853f2a06e57a81bfe
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-01-02 15:41:06 -0300

    estudando template literal

---
+++ template literal.js
@@ -0,0 +1,8 @@
+// TEMPLATE LITERAL
+const name = 'Fernanda';
+const lastName = 'Aipana'
+const email = 
+`Olá ${name} ${lastName}, tudo bem?
+Ao ver esse e-mail nos responda se puder!`
+
+console.log(email);
